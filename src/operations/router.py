47842eb6cf0372db8c7738c88107d1433a0538c5
Ilya Svyahzin <105287964+svyazhin00@users.noreply.github.com>
2023-10-04 22:42:44 +0300

    add: new router for create and select data table

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,10 +1,31 @@
-from fastapi import APIRouter
+from typing import List
+
+from fastapi import APIRouter, Depends
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.operations.shemas import OperationCreate
+from src.database import get_async_session
+from src.operations.models import operation
+
 
 router = APIRouter(
     prefix="/operations",
     tags=["Operation"]
 )
 
-@router.get("/")
-async def get_operations():
-    pass
+
+@router.get("/", response_model=List[list])
+async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
+    query = select(operation).where(operation.c.type == operation_type)
+    result = await session.execute(query)
+    return result
+
+
+@router.post("/")
+async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+    stmt = insert(operation).values(**new_operation.dict())
+    await session.execute(stmt)
+    await session.commit()
+    return {'status': 'success'}
+
